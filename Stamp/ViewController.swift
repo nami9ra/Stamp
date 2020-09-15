a54e21821fc4ac2228955699cb24f8f1b7737a58
watanabe nami <210392v@ugs.kochi-tech.ac.jp>
2020-09-15 16:22:01 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by 渡辺奈実 on 2020/09/15.
+//  Copyright © 2020 Watanabe nami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
